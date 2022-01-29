d92c72f3c842047fe5be8d668b9593dd7fb51e59
Adam <296786475@qq.com>
2022-01-29 10:24:25 +0800

    Add SkipList

--- 10-跳表/src/com/adam/Main.java
+++ 10-跳表/src/com/adam/Main.java
@@ -1,9 +1,72 @@
 package com.adam;
 
+import java.util.TreeMap;
+
+import com.adam.tools.Asserts;
+import com.adam.tools.Times;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		time();
+	}
+
+	private static void time() {
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		SkipList<Integer, Integer> list = new SkipList<>();
+		int count = 100_0000;
+
+		int delta = 10;
+
+		Times.test("SkipList", () -> {
+
+			test(list, count, delta);
+		});
+		Times.test("TreeMap", () -> {
+
+			test(map, count, delta);
+		});
+	}
+
+	private static void test1() {
+		SkipList<Integer, Integer> list = new SkipList<>();
+		test(list, 100, 5);
+		test(list, 20, 5);
+	}
+
+	private static void test(SkipList<Integer, Integer> list, int count, int delta) {
+
+		for (int i = 0; i < count; i++) {
+			list.put(i, i + delta);
+		}
+//		System.out.println(list);
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.get(i) == i + delta);
+		}
+		Asserts.test(list.size() == count);
+
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.remove(i) == i + delta);
+		}
+		Asserts.test(list.size() == 0);
+
+	}
+
+	private static void test(TreeMap<Integer, Integer> list, int count, int delta) {
+
+		for (int i = 0; i < count; i++) {
+			list.put(i, i + delta);
+		}
+//		System.out.println(list);    
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.get(i) == i + delta);
+		}
+		Asserts.test(list.size() == count);
+
+		for (int i = 0; i < count; i++) {
+			Asserts.test(list.remove(i) == i + delta);
+		}
+		Asserts.test(list.size() == 0);
 
 	}
 
--- 10-跳表/src/com/adam/SkipList.java
+++ 10-跳表/src/com/adam/SkipList.java
@@ -10,6 +10,11 @@ public class SkipList<K, V> {
 	 */
 	private static final int MAX_LEVEL = 32;
 	/**
+	 * 概率
+	 */
+	private static final double P = 0.25;
+
+	/**
 	 * 有效层数
 	 */
 	private int level;
@@ -17,8 +22,7 @@ public class SkipList<K, V> {
 	public SkipList(Comparator<K> comparator) {
 		this.comparator = comparator;
 
-		first = new Node<>();
-		first.nexts = new Node[MAX_LEVEL];
+		first = new Node<>(null, null, MAX_LEVEL);
 	}
 
 	public SkipList() {
@@ -41,6 +45,39 @@ public class SkipList<K, V> {
 
 	public V put(K key, V value) {
 		keyCheck(key);
+		Node<K, V> node = first;
+		Node<K, V>[] prevs = new Node[level];
+		for (int i = level - 1; i >= 0; i--) {
+			int cmp = -1;
+			while (node.nexts[i] != null && (cmp = compare(key, node.nexts[i].key)) > 0) {
+				node = node.nexts[i];
+			}
+			if (cmp == 0) {// 节点存在
+
+				V oldV = node.nexts[i].value;
+				node.nexts[i].value = value;
+
+				return oldV;
+			}
+			prevs[i] = node;
+		}
+		int newLevel = randomLevel();
+		// 节点不存在 添加新节点
+		Node<K, V> newNode = new Node<>(key, value, newLevel);
+		// 设置前驱和后继
+		for (int i = 0; i < newLevel; i++) {
+			if (i >= level) {
+				first.nexts[i] = newNode;
+			} else {
+				newNode.nexts[i] = prevs[i].nexts[i];
+				prevs[i].nexts[i] = newNode;
+			}
+
+		}
+		/// 节点数量增加
+		size++;
+		/// 计算跳表的最终层数
+		level = Math.max(level, newLevel);
 		return null;
 	}
 
@@ -61,7 +98,61 @@ public class SkipList<K, V> {
 
 	public V remove(K key) {
 		keyCheck(key);
-		return null;
+		Node<K, V> node = first;
+		Node<K, V>[] prevs = new Node[level];
+		boolean exist = false;
+		for (int i = level - 1; i >= 0; i--) {
+			int cmp = -1;
+			while (node.nexts[i] != null && (cmp = compare(key, node.nexts[i].key)) > 0) {
+				node = node.nexts[i];
+			}
+			if (cmp == 0) {// 节点存在
+				exist = true;
+			}
+			prevs[i] = node;
+		}
+		if (!exist) {
+			return null;
+		}
+		// 数量减少
+		size--;
+		// 需要被删除的节点
+		Node<K, V> removedNode = node.nexts[0];
+		// 设置前驱和后继
+		for (int i = 0; i < removedNode.nexts.length; i++) {
+			prevs[i].nexts[i] = removedNode.nexts[i];
+		}
+		// 跟新level
+		int newLevel = level;
+		while (--newLevel >= 0 && first.nexts[newLevel] == null) {
+			level = newLevel;
+
+		}
+		return removedNode.value;
+	}
+
+	private int randomLevel() {
+		int level = 1;
+		while (Math.random() < P && level < MAX_LEVEL) {
+			level++;
+		}
+		return level;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("一共" + level + "层").append("\n");
+		for (int i = level - 1; i >= 0; i--) {
+			Node<K, V> node = first;
+			while (node.nexts[i] != null) {
+//				sb.append("key: " + node.nexts[i].key).append(", value: " + node.nexts[i].value + " ");
+				sb.append(node.nexts[i] + " ");
+				node = node.nexts[i];
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 	private void keyCheck(K key) {
@@ -78,5 +169,17 @@ public class SkipList<K, V> {
 		K key;
 		V value;
 		Node<K, V>[] nexts;
+
+		public Node(K key, V value, int level) {
+			this.key = key;
+			this.value = value;
+			nexts = new Node[level];
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "{" + key + ":" + value + "}_" + nexts.length;
+		}
 	}
 }
